c883af364de1ed9501fb1648f8660cd856b17917
bdilbeck <89172591+bdilbeck@users.noreply.github.com>
2023-08-14 19:17:38 -0400

    Update app.py
    app.py can now receive information from the firewall script, but I'm still trying to figure out how to make it dynamically update on both the flask & firewall ends.

--- app.py
+++ app.py
@@ -1,21 +1,29 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template
+import firewallalert
+
 
 
 app = Flask(__name__)
 
+"""@app.route("/")
+def dynamic_page():
+    return firewallalert.firewall()"""
+    
+# ^ Is this the key to making the page dynamic?
 
 @app.route("/")
-
-
 def home():
-    return "<h1>Hello World! This is the Firewall Interface!</h1>"
+    return render_template('interface.html', info=firewallalert.blocked_ips)
 
 
-@app.route("/<name>")
-def user(name):
-    return f"Hello {name}!"
 
+@app.route("/admin")
+def admin():
+    #Redirects un-authenticated users from admin page
+    return redirect(url_for("home"))
 
+if __name__ == "__main__":
+    app.run()
 @app.route("/admin")
 def admin():
     #Redirects un-authenticated users from admin page
