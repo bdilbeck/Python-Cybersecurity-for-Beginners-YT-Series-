daa6629c2b7e7da03392c29fb92938509a75f6df
bdilbeck <89172591+bdilbeck@users.noreply.github.com>
2023-09-04 20:40:49 -0400

    Update app.py
    Flask app now takes information from a text file called 'blacklist.txt'.

--- app.py
+++ app.py
@@ -1,33 +1,31 @@
-from flask import Flask, redirect, url_for, render_template
-import firewallalert
+from flask import Flask, redirect, url_for, render_template, jsonify
+from flask_socketio import SocketIO
+import secrets
 
 
 
 app = Flask(__name__)
 
-"""@app.route("/")
-def dynamic_page():
-    return firewallalert.firewall()"""
-    
-# ^ Is this the key to making the page dynamic?
+secret_key = secrets.token_hex(16)
+app.config['SECRET_KEY'] = secret_key
+socketio = SocketIO(app)
 
-@app.route("/")
-def home():
-    return render_template('interface.html', info=firewallalert.blocked_ips)
-
-
+@app.route('/', methods=['GET'])
+def get_blocked_ips_():
+    with open('blacklist.txt','r') as file:
+        file_content = file.read()
+        return render_template('interface.html', content=file_content)
 
 @app.route("/admin")
 def admin():
     #Redirects un-authenticated users from admin page
     return redirect(url_for("home"))
 
-if __name__ == "__main__":
-    app.run()
-@app.route("/admin")
-def admin():
-    #Redirects un-authenticated users from admin page
-    return redirect(url_for("home"))
+@socketio.on('connect')
+def on_connect():
+    print("Client connected")
 
 if __name__ == "__main__":
     app.run()
+
+ #app.run(debug=True, port=5000)
